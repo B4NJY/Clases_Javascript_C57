8b01c17a7cbde112339c8bed5e89e26b07c37f4b
B4NJY <benjaminisraelb@gmail.com>
2022-08-01 22:31:14 -0300

    CLASE 12

---
+++ script.js
@@ -0,0 +1,85 @@
+/* TIPOS de DATOS */
+
+
+/* operdador de asignacion */
+
+/* 12+12 = 24 */
+
+/* var number = 895 ;
+
+number = number + 5 ;
+
+console.log(number) */
+
+/* var myVar;
+
+
+
+myVar = 5;
+
+
+
+var myNum;
+
+
+myNum = myVar;
+
+
+
+console.log(myNum * myVar) */
+
+
+/* var myName = 'Benjamin';
+
+var textName = " is my first name!"
+
+console.log(myName + textName ) */
+
+
+/* var a;
+var b;
+var c;
+
+
+a = a + " sring";
+
+console.log(a) */
+
+
+/* var anotherVariableName;
+
+var AnotherVariableName;
+
+var anothervariablename;
+
+
+var miNombreEsIgnacio; */
+
+/* let camper1 = "James";
+let camper = "David";
+
+
+var Camper = "Romina";
+
+console.log(camper1) */
+
+
+/* const myConstante = "Enero" */
+
+/* myConstante = "Marzo" */
+
+/* let mes = "Abril"
+
+myConstante = "febrero"
+
+
+mes = "Julio"
+
+
+console.log(mes)
+
+console.log(myConstante) */
+
+
+
+
